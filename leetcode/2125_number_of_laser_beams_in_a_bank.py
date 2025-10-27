f03b49a1e8746f603e135192e81a124a8a363643
LiuVuitton <liulihao92@gmail.com>
2025-10-27 18:18:57 +0100

    Update

---
+++ leetcode/2125_number_of_laser_beams_in_a_bank.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        prev_devices = 0
+        total_beams = 0
+        
+        for row in bank:
+            curr_devices = row.count('1')
+            if curr_devices:
+                total_beams += prev_devices * curr_devices
+                prev_devices = curr_devices
+        
+        return total_beams
